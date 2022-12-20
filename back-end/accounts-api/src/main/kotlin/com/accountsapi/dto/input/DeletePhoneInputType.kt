26da612ffa5956ccc203af1539b9d2bf3ae5e9e5
leandro.moreira <salesReportGeneratorProcessor>
2022-12-20 16:56:25 -0300

    ïnitial

---
+++ back-end/accounts-api/src/main/kotlin/com/accountsapi/dto/input/DeletePhoneInputType.kt
@@ -0,0 +1,3 @@
+package com.accountsapi.dto.input
+
+data class DeletePhoneInputType (val id: Int)
--- back-end/accounts-api/src/main/kotlin/com/accountsapi/resolver/MutationResolver.kt
+++ back-end/accounts-api/src/main/kotlin/com/accountsapi/resolver/MutationResolver.kt
@@ -3,6 +3,7 @@ package com.accountsapi.resolver
 import com.accountsapi.adapter.AccountAdapter
 import com.accountsapi.dto.input.CreateAccountInputType
 import com.accountsapi.dto.input.CreatePhoneInputType
+import com.accountsapi.dto.input.DeletePhoneInputType
 import com.accountsapi.dto.response.AccountResponseType
 import com.accountsapi.dto.response.PhoneResponseType
 import com.accountsapi.repository.AccountRepository
@@ -28,6 +29,13 @@ class MutationResolver(
         val newPhone = accountAdapter.toCreatePhone(accountRepository.findById(phone.account_id.toLong()).get(), phone)
         phoneRepository.save(newPhone)
         return accountAdapter.toPhone(newPhone)
+    }
+
+    fun deletePhone(phone: DeletePhoneInputType): Boolean {
+
+        phoneRepository.deleteById(phone.id.toLong())
+        return phoneRepository.findById(phone.id.toLong()).isEmpty
+
     }
 
 }
